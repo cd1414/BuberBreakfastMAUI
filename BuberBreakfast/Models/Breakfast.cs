ad264ee64b8f47e4ee1aa0a5896bd89fc8fc7702
Christian <cd1414@gmail.com>
2022-11-25 10:53:42 -0600

    Start app development

---
+++ BuberBreakfast/Models/Breakfast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BuberBreakfast.Models
+{
+    public record Breakfast(
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EndDataTime,
+        Uri Image,
+        List<string> Savory,
+        List<string> Sweet);
+}
+
---
+++ BuberBreakfast/ViewModel/MyBreakfastViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BuberBreakfast.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BuberBreakfast.ViewModel
+{
+    public partial class MyBreakfastViewModel
+        : ObservableObject
+    {
+        [ObservableProperty]
+        List<Breakfast> breakfasts;
+
+        public MyBreakfastViewModel()
+        {
+            LoadBreakfasts();
+        }
+
+        private void LoadBreakfasts()
+        {
+            Breakfasts = new()
+            {
+                new Breakfast(
+                    Name: "Vegan Sunshine",
+                    Description: "Vegan everything",
+                    StartDateTime: DateTime.Now.AddDays(1),
+                    EndDataTime: DateTime.Now.AddDays(1).AddHours(2),
+                    Image: new Uri(""),
+                    Savory: new List<string> { "Oatmeal", "Avocado Toas"},
+                    Sweet: new List<string> { "Cookie"}
+                    ;
+        }
+    }
+}
+
